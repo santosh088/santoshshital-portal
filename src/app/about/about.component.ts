16d59ccbf848da9dc1ebcb26428d371c7707691f
santosh088 <skrai8100@gmail.com>
2023-07-23 11:42:24 +0530

    added images for sliding and fix other issues

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,41 +12,41 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
   imageObject = [{
-    image: 'https://github.com/santosh088/santoshshital-portal/blob/master/assets/images/weightloss/1.jpeg',
-    thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/1.jpeg',
+    image: 'https://santosh088.github.io/weightloss-images/1.jpeg',
+    thumbImage: 'https://santosh088.github.io/weightloss-images/1.jpeg',
 }, {
-    image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/2.jpeg',
-    thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/2.jpeg'
+    image: 'https://santosh088.github.io/weightloss-images/2.jpeg',
+    thumbImage: 'https://santosh088.github.io/weightloss-images/2.jpeg'
 }, {
-    image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/4.jpeg',
-    thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/4.jpeg',
+    image: 'https://santosh088.github.io/weightloss-images/4.jpeg',
+    thumbImage: 'https://santosh088.github.io/weightloss-images/4.jpeg',
 },{
-    image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/7.jpeg',
-    thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/7.jpeg',
+    image: 'https://santosh088.github.io/weightloss-images/7.jpeg',
+    thumbImage: 'https://santosh088.github.io/weightloss-images/7.jpeg',
 }, {
-    image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/5.jpeg',
-    thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/5.jpeg'
+    image: 'https://santosh088.github.io/weightloss-images/5.jpeg',
+    thumbImage: 'https://santosh088.github.io/weightloss-images/5.jpeg'
 }, {
-    image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/8.jpeg',
-    thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/8.jpeg',
+    image: 'https://santosh088.github.io/weightloss-images/8.jpeg',
+    thumbImage: 'https://santosh088.github.io/weightloss-images/8.jpeg',
 },{
-  image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/1.jpeg',
-  thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/1.jpeg',
+  image: 'https://santosh088.github.io/weightloss-images/1.jpeg',
+  thumbImage: 'https://santosh088.github.io/weightloss-images/1.jpeg',
 }, {
-  image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/2.jpeg',
-  thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/2.jpeg'
+  image: 'https://santosh088.github.io/weightloss-images/2.jpeg',
+  thumbImage: 'https://santosh088.github.io/weightloss-images/2.jpeg'
 }, {
-  image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/4.jpeg',
-  thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/4.jpeg',
+  image: 'https://santosh088.github.io/weightloss-images/4.jpeg',
+  thumbImage: 'https://santosh088.github.io/weightloss-images/4.jpeg',
 },{
-  image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/7.jpeg',
-  thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/7.jpeg',
+  image: 'https://santosh088.github.io/weightloss-images/7.jpeg',
+  thumbImage: 'https://santosh088.github.io/weightloss-images/7.jpeg',
 }, {
-  image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/5.jpeg',
-  thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/5.jpeg'
+  image: 'https://santosh088.github.io/weightloss-images/5.jpeg',
+  thumbImage: 'https://santosh088.github.io/weightloss-images/5.jpeg'
 }, {
-  image: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/8.jpeg',
-  thumbImage: 'https://github.com/santosh088/santoshshital-portal/tree/master/assets/images/weightloss/8.jpeg',
+  image: 'https://santosh088.github.io/weightloss-images/8.jpeg',
+  thumbImage: 'https://santosh088.github.io/weightloss-images/8.jpeg',
 }
 ];
 
